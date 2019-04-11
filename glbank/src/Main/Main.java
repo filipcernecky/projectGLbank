3795c21479e798a7608406885c82fc1b5c7112e0
filipcernecky <43435930+filipcernecky@users.noreply.github.com>
2019-04-11 12:41:11 +0200

    Add files via upload

--- glbank/src/Main/Main.java
+++ glbank/src/Main/Main.java
@@ -1,25 +1,5 @@
 package Main;
 
-//import Database.Database;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+public class Main {
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("GLB");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-//wip
-        launch(args);
-    }
 }
